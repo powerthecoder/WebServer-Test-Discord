57d843b45727bf6f40553724bf548d0c9df95ca8
powerthecoder <68101457+powerthecoder@users.noreply.github.com>
2020-10-04 00:03:55 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 from flask import Flask, render_template, request
 import os
 import sys
+import json
 
 
 app = Flask('__main__')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if (request.method == "POST"):
+        if (request.form['discord_test'] == "discord_test"):            
+            os.system('python3 discord_test.py')
+        else:
+            pass
+    else:
+        return render_template('index.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="127.0.0.1")
